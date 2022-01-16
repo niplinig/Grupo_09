89c5bf2d11fc6ae1d5da511400b49a68665db36a
Nicolás Andrés Plaza Iñiguez <niplinig@espol.edu.ec>
2022-01-15 21:35:04 -0500

    In MatchPvsPc is always player who choose the sign

--- src/main/java/main/ChooseXorOController.java
+++ src/main/java/main/ChooseXorOController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import match.MatchPvPC;
 import signs.SignManager;
 
 public class ChooseXorOController implements Initializable {
@@ -25,23 +26,42 @@ public class ChooseXorOController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         App.getMatch().setSignManger(new SignManager());
-        txtTitle.setText("Choose " + App.getMatch().getFirst().getName() + " sign");
-        bttn1.setText(App.getMatch().getSign());
-        bttn2.setText(App.getMatch().getSign());
+
+        if (App.getMatch() instanceof MatchPvPC) {
+            txtTitle.setText("Choose " + App.getMatch().getPlayer1().getName() + " sign");
+            bttn1.setText(App.getMatch().getSign());
+            bttn2.setText(App.getMatch().getSign());
+        } else {
+            txtTitle.setText("Choose " + App.getMatch().getFirst().getName() + " sign");
+            bttn1.setText(App.getMatch().getSign());
+            bttn2.setText(App.getMatch().getSign());
+        }
     }
 
     @FXML
     private void bttn1Clicked(MouseEvent event) throws IOException {
-        App.getMatch().getFirst().setSign(bttn1.getText());
-        App.getMatch().getSecond().setSign(bttn2.getText());
-        App.setRoot("game");
+        if (App.getMatch() instanceof MatchPvPC) {
+            App.getMatch().getPlayer1().setSign(bttn1.getText());
+            App.getMatch().getPlayer2().setSign(bttn2.getText());
+            App.setRoot("game");
+        } else {
+            App.getMatch().getFirst().setSign(bttn1.getText());
+            App.getMatch().getSecond().setSign(bttn2.getText());
+            App.setRoot("game");
+        }
     }
 
     @FXML
     private void bttn2Clicked(MouseEvent event) throws IOException {
-        App.getMatch().getFirst().setSign(bttn2.getText());
-        App.getMatch().getSecond().setSign(bttn1.getText());
-        App.setRoot("game");
+        if (App.getMatch() instanceof MatchPvPC) {
+            App.getMatch().getPlayer1().setSign(bttn2.getText());
+            App.getMatch().getPlayer2().setSign(bttn1.getText());
+            App.setRoot("game");
+        } else {
+            App.getMatch().getFirst().setSign(bttn2.getText());
+            App.getMatch().getSecond().setSign(bttn1.getText());
+            App.setRoot("game");
+        }
     }
 
     @FXML
